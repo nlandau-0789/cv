a426343a8f5e0b874cc96eacbaabc981081c8193
nlandau-0789 <84878415+nlandau-0789@users.noreply.github.com>
2023-06-05 15:40:24 +0200

    base

---
+++ scripts.js
@@ -0,0 +1,16 @@
+if (document.querySelector('details')) {
+    // Fetch all the details elements
+    const details = document.querySelectorAll('details');
+
+    // Add onclick listeners
+    details.forEach((targetDetail) => {
+        targetDetail.addEventListener("click", () => {
+            // Close all details that are not targetDetail
+            details.forEach((detail) => {
+                if (detail !== targetDetail) {
+                    detail.removeAttribute("open");
+                }
+            });
+        });
+    });
+}
